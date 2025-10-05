10c486ad3e9dee3654b40c382fe7c05433c6a8ed
Mirjalal Seyidli <mircalalseyidli2005@gmail.com>
2025-10-05 11:58:15 +0400

    final for now)

--- src/components/features/shared/sign-out-button/index.tsx
+++ src/components/features/shared/sign-out-button/index.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import {useTranslations} from "next-intl";
 
 export default function SignOutButton()
 {
+    const t = useTranslations('navigation');
     const callbackUrl = '/';
     const handleClick = () => {
         signOut({callbackUrl});
@@ -11,7 +13,7 @@ export default function SignOutButton()
 
     return (
         <button type="button" onClick={handleClick} className="inline-flex items-center rounded-xl px-4 py-2 text-[16px] font-semibold text-[#1f1d2a] shadow-sm bg-[#f6d44b] cursor-pointer">
-            Sign Out
+            {t("signOut")}
         </button>
     );
 }
